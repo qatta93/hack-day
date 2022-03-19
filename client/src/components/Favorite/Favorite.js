d34d25badad30cb89a527a7508f615a27a96434e
patrycja panasiuk <panasiuk.patrycja@gmail.com>
2022-03-19 09:21:41 +0100

    project finished

--- client/src/components/Favorite/Favorite.js
+++ client/src/components/Favorite/Favorite.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './Favorite.css';
 
 const Favorite = ({favList, setFavList}) => {
@@ -11,7 +12,7 @@ const resetFavList = () => {
       <section className='favorite'>
         <div className='favorite__wrapper'>
           <h1 className='favorite__title'>Your Favorite Destinations:</h1>
-          {favList.map(fav => <h2 className='favorite__country' key={fav}>{fav}</h2>)}
+          {favList.map(fav => <h2 className='favorite__country' id={uuidv4()} key={fav}>{fav}</h2>)}
           <button className='favorite__reset' onClick={resetFavList}></button>
         </div>
       </section>
